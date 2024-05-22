20489b57d64ecdb1b56f89f547f33ef2092cd830
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-05-21 22:43:28 -0300

    Estilização inicial no cabeçalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { CardGameComponent } from './components/card-game/card-game.component';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 import { CardGameComponent } from './card-game/card-game.component';
 import { ButtonShowMoreComponent } from './button-show-more/button-show-more.component';
 
 @NgModule({
-  declarations: [HeaderComponent, CardGameComponent, ButtonShowMoreComponent],
+  declarations: [CardGameComponent, ButtonShowMoreComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, CardGameComponent, ButtonShowMoreComponent],
+  exports: [CardGameComponent, ButtonShowMoreComponent],
 })
 export class SharedComponesModule {}
