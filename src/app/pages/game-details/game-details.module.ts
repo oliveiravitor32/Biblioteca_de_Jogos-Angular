aa0ea800deca8d0752302ca07db6c97214fabcb0
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-05-26 13:58:48 -0300

    Adiciona mais informações para a página de detalhe de jogo e melhora estilização

--- src/app/pages/game-details/game-details.module.ts
+++ src/app/pages/game-details/game-details.module.ts
@@ -5,9 +5,10 @@ import { CommonModule } from '@angular/common';
 import { GameDetailsRoutingModule } from './game-details-routing.module';
 import { GameDetailsComponent } from './game-details.component';
 import { SharedDirectivesModule } from '../../directives/shared-directives.module';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
-  declarations: [GameDetailsComponent],
+  declarations: [GameDetailsComponent, CarouselComponent],
   imports: [
     CommonModule,
     GameDetailsRoutingModule,
