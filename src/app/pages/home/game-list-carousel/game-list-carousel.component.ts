62660c2da82c013b78f782c3202e09a6b749d152
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-06-26 11:32:40 -0300

    Conserta carrosel da página de início

--- src/app/pages/home/game-list-carousel/game-list-carousel.component.ts
+++ src/app/pages/home/game-list-carousel/game-list-carousel.component.ts
@@ -1,4 +1,5 @@
 import {
+  AfterViewInit,
   Component,
   ElementRef,
   EventEmitter,
@@ -13,8 +14,9 @@ import { IGameOverview } from '../../../interfaces/game/game-overview.interface'
   templateUrl: './game-list-carousel.component.html',
   styleUrl: './game-list-carousel.component.scss',
 })
-export class GameListCarouselComponent {
+export class GameListCarouselComponent implements AfterViewInit {
   @ViewChild('carouselInner') carouselInnerEl!: ElementRef<HTMLDivElement>;
+  @ViewChild('carouselItem') carouselItemEl!: ElementRef<HTMLDivElement>;
 
   @Input() gameList: IGameOverview[] = [];
 
@@ -25,21 +27,56 @@ export class GameListCarouselComponent {
     this.selectGameEmmiter.emit(game);
   }
 
-  carouselInnerTranslate: number = 0;
+  ngAfterViewInit(): void {
+    this.itemWidth = this.carouselItemEl.nativeElement.offsetWidth;
+
+    this.totalItems = this.gameList.length;
+  }
+
+  itemWidth: number = 0;
+  totalItems: number = 0;
+
+  visibleItems: number = 4;
+  currentIndex: number = 0;
+
+  displacement: number = 0;
 
   previousSlide() {
-    if (this.carouselInnerTranslate < 0) {
-      this.carouselInnerEl.nativeElement.style.transform = `translateX(${
-        this.carouselInnerTranslate + 50
-      }%)`;
-      this.carouselInnerTranslate += 50;
+    //tratamento para momento em que o movimento passaria do limite do array
+    if (this.currentIndex - this.visibleItems < 0) {
+      //tratamento para lidar com os index 1 2 3, pois o slide normalmente passaria do limite e iria para o final
+      if (this.currentIndex !== 0) {
+        console.log(this.currentIndex);
+        this.currentIndex = 0;
+        this.displacement = this.itemWidth * this.currentIndex;
+        //tratamento para lidar com limite normal de movimentação do slide indo para o final da lista
+      } else {
+        this.currentIndex = this.totalItems;
+        this.displacement = this.itemWidth * this.totalItems;
+      }
+      // tratamento normal para voltar slide
+    } else {
+      this.displacement -= this.itemWidth * this.visibleItems;
+      this.currentIndex -= this.visibleItems;
     }
+
+    this.carouselInnerEl.nativeElement.style.transform = `translateX(${-this
+      .displacement}px)`;
   }
 
   nextSlide() {
-    this.carouselInnerEl.nativeElement.style.transform = `translateX(${
-      this.carouselInnerTranslate - 50
-    }%)`;
-    this.carouselInnerTranslate -= 50;
+    // tratamento para vefiricar quando a movimentação do slide passaria do limite
+    if (this.currentIndex + this.visibleItems > this.totalItems) {
+      this.currentIndex = 0;
+      this.displacement = 0;
+
+      // tratamento normal para movimentação do slide
+    } else {
+      this.displacement += this.itemWidth * this.visibleItems;
+      this.currentIndex += this.visibleItems;
+    }
+
+    this.carouselInnerEl.nativeElement.style.transform = `translateX(${-this
+      .displacement}px)`;
   }
 }
