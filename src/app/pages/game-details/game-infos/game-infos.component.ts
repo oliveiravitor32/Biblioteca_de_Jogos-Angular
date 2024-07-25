126850d972ac821032b24e80600158c1220e473b
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-07-25 12:05:20 -0300

    Remove lista de gêneros em detalhes de jogo e cria pipe para lidar com string de requerimentos da plataforma PC vinda da API

--- src/app/pages/game-details/game-infos/game-infos.component.ts
+++ src/app/pages/game-details/game-infos/game-infos.component.ts
@@ -11,10 +11,27 @@ export class GameInfosComponent implements OnChanges {
 
   loading: boolean = true;
 
+  enablePCRequirements: boolean = true;
+
+  minimunRequerimentsForPC: string = '';
+  recommendedRequerimentsForPC: string = '';
+
   ngOnChanges(): void {
     // atualizar carregamento quando a requisição do jogo for completa e evitar erros de leitura (Cannot read properties of undefined (reading '0'))
     // descrição somente é recebida junto a requisição de detalhes sobre o jogo
     if (this.game.description) {
+      // Verificar existência da plataforma PC no jogo selecionado
+      for (let platform of this.game.platforms) {
+        const isOnPc = platform.platform.name.toLowerCase().includes('pc');
+        if (isOnPc) {
+          this.enablePCRequirements = true;
+          this.minimunRequerimentsForPC = platform.requirements.minimum;
+          this.recommendedRequerimentsForPC = platform.requirements.recommended;
+
+          console.log(platform);
+        }
+      }
+
       this.loading = false;
     }
   }
