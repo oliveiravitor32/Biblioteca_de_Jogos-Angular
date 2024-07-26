b7db236c98f75e511c5cdeed98b1bf552f5d2538
oliveiravitor32 <oliveiravitor327@gmail.com>
2024-07-26 12:35:12 -0300

    Cria responsividade para o cabeçalho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss', './app.component.responsive.scss'],
 })
 export class AppComponent {
   title = 'GameLibrary';
